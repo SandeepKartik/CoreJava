18d44331353ba3c5b8071590993eb19b4a99eab0
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 17:32:53 +0530

    AssignmentOperator Exmple

---
+++ Operators/AssignmentOperator.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        /* Assignment Operator is used to 
+        Assign the value to the variable or another way variable to variable  */
+        int a=2508;
+        System.out.println(a);
+        int b=a;
+        System.out.println(b);
+    }
+    
+}
