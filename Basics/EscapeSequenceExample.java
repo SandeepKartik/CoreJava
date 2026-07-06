20103aa9e5f5cd99710ae955256b205a2126458e
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:50:12 +0530

    EscapeSequence Example

---
+++ Basics/EscapeSequenceExample.java
@@ -0,0 +1,15 @@
+public class EscapeSequenceExample {
+    public static void main(String[] args) {
+        /*Scape sequence escape sequences are special character combinations used to represent characters that are otherwise difficult or impossible to type directly, or that have a special meaning in the language (like the double quote used to define a string) */
+
+        System.out.println("Hello\nJava");
+
+        System.out.println("Hello\tJava");
+
+        System.out.println("\"Java\"");
+
+        System.out.println("\\Java");
+        System.out.print("This\f is my first program");
+    }
+    
+}
