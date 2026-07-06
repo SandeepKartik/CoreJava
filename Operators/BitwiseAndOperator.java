682bbdaed6e520ea221092e91dd93265402fb7bc
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 21:49:06 +0530

    OutputOnAndOperator

---
+++ Operators/BitwiseAndOperator.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class BitwiseAndOperator {
+    public static void main(String[] args) {
+        // find number output 
+        int a=10;
+        int b=20;
+        System.out.println(a&b);
+    }
+    
+}
