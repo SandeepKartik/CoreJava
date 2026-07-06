3ebf750c3306ee5cb965b1b5af7a9961e96e44b1
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:30:00 +0530

    Variable Exampls

---
+++ Basics/VariablesExamples.java
@@ -0,0 +1,17 @@
+public class VariablesExamples {
+    public static void main(String[] args) {
+        
+    
+    // Variable is Referance of memory location where element is stored . 
+    // its following multiple types 
+    // declaration of variable 
+    int a =10;
+    String s="Sam Kartik";
+    float f=1.01f;
+    double d=1.003;
+    System.out.println("Integer "+a);
+    System.out.println("String Value " +s);
+    System.out.println("Float "+f);
+    System.out.println("Double value : "+d);
+    }
+}
