b24e395aa908ff19fd44031e6b5e74b4018a1d5c
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:41:56 +0530

    TypeCastingExample

---
+++ Basics/TypeCastingExample.java
@@ -0,0 +1,19 @@
+public class TypeCastingExample {
+    public static void main(String[] args) {
+        /*Type casting means change data from one data type to another 
+        its maily two type  Widening Casting (Implicit) and Narrowing Casting (Explicit) */
+
+        int num = 100;
+
+        double d = num;
+
+        System.out.println(d);
+
+        double x = 25.75;
+
+        int y = (int) x;
+
+        System.out.println(y);
+    }
+    
+}
