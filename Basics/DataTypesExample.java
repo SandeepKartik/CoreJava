5b1cde6061ff1ae9b7f403fcf37d8142abea3eaa
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:36:05 +0530

    DataTypes Example

---
+++ Basics/DataTypesExample.java
@@ -0,0 +1,26 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        /*Data types mainly tell us what type data will be stored in memory its define behaivoures  */
+        byte b = 100;
+        short s = 2000;
+        int i = 50000;
+        long l = 9000000000L;
+
+        float f = 10.5f;
+        double d = 20.55;
+
+        char c = 'A';
+
+        boolean flag = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(flag);
+    }
+    
+}
