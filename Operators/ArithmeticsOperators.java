8e78a79e7d3eafc4cd624afec64ee62eb5af0cee
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 17:20:33 +0530

    ArithmeticsOperator Example

---
+++ Operators/ArithmeticsOperators.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class ArithmeticsOperators {
+    public static void main(String[] args) {
+        /* The Arithmetics operators are used to preform Arithmetics operations like addition , 
+        Subtraction , etc   */
+        int a=10;
+        int b=21;
+        System.out.println(a+b);// addition
+        System.out.println(b-a); //Subtraction
+        System.out.println(a*b); //Multipliction
+        System.out.println(b/a); //Division 
+        System.out.println(b%a); //Modulus give reminder 
+
+    }
+    
+}
