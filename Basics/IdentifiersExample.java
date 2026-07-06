fbf09c2858efc39903d368f5f876cc78f7a54c66
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:54:40 +0530

    Identifiers Examples

---
+++ Basics/IdentifiersExample.java
@@ -0,0 +1,11 @@
+public class IdentifiersExample {
+    
+     public static void main(String[] args) {
+        /*Identifiers are the name giving to the various elements such as variable , class methods ets  */
+        int studentAge = 22;
+        String studentName = "Sam";
+        System.out.println(studentAge);
+        System.out.println(studentName);
+    }
+    
+}
