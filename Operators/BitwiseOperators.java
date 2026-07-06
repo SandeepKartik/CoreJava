15ef304ba8663a89803080b2445acbb23f23ff2c
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 21:41:45 +0530

    BitwiseOperator Theory

---
+++ Operators/BitwiseOperators.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        /*Bitwise operators perform operations on 
+        the binary (bit) representation of integer values. */
+        /*
+        1. & - Bitwise and Operator 
+        2 BitWise Or Operator - |
+        3. Bitwise xor Operator - ^
+        4. Bitwise Left Shift Operator - <<
+        5. Bitwise Right Shift Operator  - >>
+        6. Unsigned Right Shift Operator -  >>>
+         */
+    }
+    
+}
