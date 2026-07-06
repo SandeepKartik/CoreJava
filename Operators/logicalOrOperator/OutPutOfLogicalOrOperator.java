350910d7162d0a337cdebe462a89fd6b4ff4a059
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 22:18:14 +0530

    Or Logicl Operatore Simple OutPut by taking input from user

---
+++ Operators/logicalOrOperator/OutPutOfLogicalOrOperator.java
@@ -0,0 +1,16 @@
+package Operators.logicalOrOperator;
+
+import java.util.Scanner;
+
+public class OutPutOfLogicalOrOperator {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Your First Number : ");
+        int num1=sc.nextInt();
+        System.out.println("Enter your secound number : ");
+        int num2=sc.nextInt();
+        int result=(num1|num2);
+        System.out.println("The result of Or Operator : "+result);
+    }
+    
+}
