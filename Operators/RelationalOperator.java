ce7a69b43d1b19a78114f2ec41da3fb5ebccbb44
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 19:11:46 +0530

    RelationalOperator Example

---
+++ Operators/RelationalOperator.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        /*These Operator is use to compair two or more operand 
+        ex- >,< ,=> etc. */
+        int a=10;
+        int b=20;
+        System.out.println(a>b); // false, a Gratter then B compairing
+        System.out.println(a<b); // true , it will be A less then B 
+        System.out.println(a>=b); // false , A is gratter or equal to B
+        System.out.println(a<=b); // true , A less than or equal to B
+        System.out.println(a!=b);// true , a not equal to b
+        System.out.println(a==b); // false , a equal to b
+    }
+    
+}
