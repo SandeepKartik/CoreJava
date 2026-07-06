1e543166665507f6a70498356112fb878d785104
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:44:43 +0530

    Operators Examples

---
+++ Basics/OperatorsExample.java
@@ -0,0 +1,15 @@
+public class OperatorsExample {
+    public static void main(String[] args) {
+        /*Operator are symbols that will be used for Preform Some tasks on operends  */
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+    
+}
