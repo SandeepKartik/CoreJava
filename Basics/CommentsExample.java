73c635b56ac6f051915a77aa55ba2b5ddd6b85f8
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:21:06 +0530

    Example  Of  Comments

---
+++ Basics/CommentsExample.java
@@ -0,0 +1,13 @@
+public class CommentsExample {
+    public static void main(String [] args){
+        //Single Line comment 
+        System.out.println("Single Line comments ");
+        /*Multi Line comment 
+        it will be comment multi line   */
+        System.out.println("Multi line comments ");
+        /**Documents Comments -- it will be create html api for documentation */
+        System.out.println("Document Comments ");
+
+    }
+    
+}
