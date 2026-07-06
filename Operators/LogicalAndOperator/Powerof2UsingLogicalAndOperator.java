7625be4b59bdc63b6dcab9fa877fec1033c96f34
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 22:10:09 +0530

    Power of 2 using Logical And Opertor

---
+++ Operators/LogicalAndOperator/Powerof2UsingLogicalAndOperator.java
@@ -0,0 +1,15 @@
+package Operators.LogicalAndOperator;
+import java.util.Scanner;
+public class Powerof2UsingLogicalAndOperator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Number : ");
+        int num=sc.nextInt();
+        if(num>0 &&(num &(num-1))==0){
+            System.out.println(" The number is Power of 2");
+
+        }else{
+            System.out.println("The number is not power of 2 ");
+        }
+    }   
+}
