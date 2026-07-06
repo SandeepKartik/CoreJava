f43867429fcc59a5c4b9534dd2c6c677e27549a7
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 17:13:16 +0530

    Operator Example

---
+++ Operators/Operator.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Operator {
+    public static void main(String[] args) {
+        /*An operator is a Symbols that Preform Operation . An operator act on some variable 
+        called Operands to get some result */
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+}
