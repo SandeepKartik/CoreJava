c3d48be1b6e4ba7c31eddcef4af60913fbefa043
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 17:02:46 +0530

    Naming Convantion examples

---
+++ Basics/NamingConventionExample.java
@@ -0,0 +1,17 @@
+public class NamingConventionExample {
+    public static void main(String[] args) {
+        /*
+        Java Naming Conventions:
+         1. Package names      -> lowercase
+         2. Class names      -> PascalCase (Start with Capital Letter
+         3. Method names       -> camelCase (Start with small letter)
+         4. Variable names     -> camelCase (Start with small letter)
+         5. Constant names     -> UPPER_CASE
+         */
+        String studentName = "Sam";
+        int studentAge = 22;
+        System.out.println(studentName);
+        System.out.println(studentAge);
+    }
+    
+}
