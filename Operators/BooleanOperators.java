0c008ba039d36b5ab867f9f3c03596afd1e27180
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 21:34:45 +0530

    BooleanOperators Example

---
+++ Operators/BooleanOperators.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class BooleanOperators {
+    public static void main(String[] args) {
+        /* A Boolean Operators act on bollean variable and produce boolean type result */
+         boolean isJavaEasy = true;
+        boolean isPythonHard = false;
+
+        System.out.println("Is Java Easy? " + isJavaEasy);
+        System.out.println("Is Python Hard? " + isPythonHard);
+    }
+    
+}
