a3d704303c1b089d4bc3857626a7b2d0054333bf
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-06 16:51:35 +0530

    PrintMethods Example

---
+++ Basics/PrintMethodsExample.java
@@ -0,0 +1,12 @@
+public class PrintMethodsExample {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello ");
+
+        System.out.println("Java");
+
+        System.out.printf("Age : %d",22);
+    }
+    
+}
