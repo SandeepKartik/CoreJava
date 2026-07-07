11783ccdce952089ac3de2fe89ea4274c46165af
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-08 02:42:51 +0530

    First Program Of While Loop

---
+++ controlStatement/whileloop/WhileBasics.java
@@ -0,0 +1,14 @@
+package controlStatement.whileloop;
+
+public class WhileBasics {
+    public static void main(String[] args) {
+        // Print from 1 to 10;
+        int i=1;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+            
+        }
+    }
+    
+}
