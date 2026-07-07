4f5c70bccb94ae4485d15a1fb4fef8171cbc1651
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-08 01:51:13 +0530

    Gratter Between Two Number Using Ternary Operator

---
+++ ternaryOperator/SimpleTernaryOperator.java
@@ -0,0 +1,11 @@
+package ternaryOperator;
+
+public class SimpleTernaryOperator {
+    public static void main(String[] args) {
+        //Check Wheter which number is gratter
+        int num1=10;
+        int num2=20;
+        System.out.println((num1>num2?num1:num2));
+    }
+    
+}
