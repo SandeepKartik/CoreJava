1a703306099a1c20ac22dc968f5c639d0f505786
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-08 02:46:13 +0530

    Reverse from 10 to 1 By Using While Loop

---
+++ controlStatement/whileloop/ReversLoop.java
@@ -0,0 +1,12 @@
+package controlStatement.whileloop;
+
+public class ReversLoop {
+    public static void main(String[] args) {
+        int num=10;
+        while (num>=1) {
+            System.out.println(num);
+            num--;
+        }
+    }
+    
+}
