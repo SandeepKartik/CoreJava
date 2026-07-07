e67283e02af558be6a1dc5648c426f05a4bd7418
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-08 01:19:04 +0530

    Left Shift Bitewise Operator Example

---
+++ operators/bitwiseLeftShiftOperator/LeftShiftOperator.java
@@ -0,0 +1,11 @@
+package operators.bitwiseLeftShiftOperator;
+
+public class LeftShiftOperator {
+    public static void main(String[] args) {
+        int a=10;
+        // left shift by 2
+        //Its known as Multiplication
+        System.out.println(a<<2);
+    }
+    
+}
