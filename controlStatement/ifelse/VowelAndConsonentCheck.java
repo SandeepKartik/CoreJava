de0b727be1f070898158dbc4131a629d6c8a80e0
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-08 07:05:33 +0530

    Vowel And Consonet Check

---
+++ controlStatement/ifelse/VowelAndConsonentCheck.java
@@ -0,0 +1,18 @@
+package controlStatement.ifelse;
+
+import java.util.Scanner;
+
+public class VowelAndConsonentCheck {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your character : ");
+        char ch=sc.next().charAt(0);
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+        ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            System.out.println("Vowel");
+        }else{
+            System.out.println("Consonent");
+        }
+    }
+    
+}
