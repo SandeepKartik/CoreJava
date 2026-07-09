86dfdccda9b6379ab3f2aacf7c785bf3d426f461
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-09 16:56:11 +0530

    print from 1 to n

---
+++ controlStatement/whileloop/PrintFrom1toN.java
@@ -0,0 +1,17 @@
+package controlStatement.whileloop;
+
+import java.util.Scanner;
+
+public class PrintFrom1toN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Positive Integer : ");
+        int n=sc.nextInt();
+        int i=1;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
