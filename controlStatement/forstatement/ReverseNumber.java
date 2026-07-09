ab2f57e586bed28a12cc6e42e14b721e0850fe83
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-10 00:03:02 +0530

    Reverse Number

---
+++ controlStatement/forstatement/ReverseNumber.java
@@ -0,0 +1,21 @@
+package controlStatement.forstatement;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int num=sc.nextInt();
+        int org_num=num;
+        int rev=0;
+        for(;num>0;){
+            int rem=num%10;
+            rev =rev*10+rem;
+            num /=10;
+        }
+
+        System.out.println("The reverse number of  "+org_num+" is "+rev);
+    }
+    
+}
