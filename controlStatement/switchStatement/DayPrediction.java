9593fd1fccae1db85142a2a9cda559c8f6eee7b9
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-09 11:11:50 +0530

    Day Prediction by using switch

---
+++ controlStatement/switchStatement/DayPrediction.java
@@ -0,0 +1,38 @@
+package controlStatement.switchStatement;
+
+import java.util.Scanner;
+
+public class DayPrediction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int day=sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;                    
+            default:
+                System.out.println("Invalid Number entered ");
+                break;
+        }
+    }
+    
+}
