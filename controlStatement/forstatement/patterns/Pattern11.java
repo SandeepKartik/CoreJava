36d1e8be86b136baf759fd05c08bf0725b107ab2
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-10 00:53:06 +0530

    Pattern 10

---
+++ controlStatement/forstatement/patterns/Pattern11.java
@@ -0,0 +1,23 @@
+package controlStatement.forstatement.patterns;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+/*
+output ::
+1
+21
+321
+4321
+54321
+
+
+*/
