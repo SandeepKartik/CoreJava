5b3b526c4527feab80222f3163960bfba984a85f
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-09 19:30:36 +0530

    Pattern Number 4 using while loop

---
+++ controlStatement/whileloop/pattern/PatternNumber4.java
@@ -0,0 +1,29 @@
+package controlStatement.whileloop.pattern;
+
+public class PatternNumber4 {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=5) {
+            int j=5;
+            while (j>=(6-i)) {
+                System.out.print(j);
+                j--;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    
+}
+/*
+OutPut :--
+
+5
+54
+543
+5432
+54321
+
+
+
+*/
