09d893c2641ba894ff6292be4c2a90e149c8d4c5
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-09 19:03:46 +0530

    Right Triangl Patter using while loop

---
+++ controlStatement/whileloop/pattern/RightTriangle.java
@@ -0,0 +1,25 @@
+package controlStatement.whileloop.pattern;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=5){
+            int j=1;
+            while (j<=i) {
+                System.out.print(" * ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    
+}
+/*OutPut=
+ * 
+ *  * 
+ *  *  * 
+ *  *  *  * 
+ *  *  *  *  * 
+ 
+ */
