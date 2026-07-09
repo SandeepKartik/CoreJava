f0dd6db4592bffe897c355bf8fecd8289a66a94b
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-10 00:41:58 +0530

    Pattern 8

---
+++ controlStatement/forstatement/patterns/Pattern8.java
@@ -0,0 +1,25 @@
+package controlStatement.forstatement.patterns;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+/*
+
+Output ::
+
+11111
+2222
+333
+44
+5
+
+
+*/
