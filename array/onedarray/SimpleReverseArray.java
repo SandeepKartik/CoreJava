b60947b61c0e74e5bc9ae70b2d4a12fc42b5385a
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-10 01:53:43 +0530

    simple reverse number

---
+++ array/onedarray/SimpleReverseArray.java
@@ -0,0 +1,17 @@
+package array.onedarray;
+
+public class SimpleReverseArray {
+    public static void main(String[] args) {
+        int [] arr={10,20,33,44,56,76,89,97};
+        int reverse [] =new int[arr.length];
+        int j=0;
+        for(int i=arr.length-1;i>=0;i--){
+            reverse[j]=arr[i];
+            j++;
+        }
+        for(int num : reverse){
+            System.out.println(num +" ");
+        }
+    }
+    
+}
