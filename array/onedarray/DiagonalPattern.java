6cc610724939128e594d07fda5e4bb18a2493051
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-10 17:45:37 +0530

    Diagonal Pattern Printing

---
+++ array/onedarray/DiagonalPattern.java
@@ -0,0 +1,28 @@
+public class DiagonalPattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j||j+i==n+1){
+                    System.out.print("* ");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
+/*
+
+OutPut :: 
+
+*    * 
+ *  *  
+  *   
+ *  *  
+*    * 
+
+
+*/
