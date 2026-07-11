34a80e789dca9fe1add017a5d57f577df63129e1
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-12 02:06:13 +0530

    Use Of Return keyword

---
+++ method/UseOfReturn.java
@@ -0,0 +1,12 @@
+package method;
+
+public class UseOfReturn {
+    int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        UseOfReturn s=new UseOfReturn();
+        System.out.println(s.add(10, 20));
+    }
+    
+}
