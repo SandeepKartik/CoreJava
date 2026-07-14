92c77c295faa6b4ee87124ecedbe6044500540b6
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-14 20:53:38 +0530

    factorial through recursion

---
+++ method/recursion/Factorial.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Factorial {
+    static int factorial(int n){
+        if(n==1){
+            return 1;
+        }
+         
+        return n*factorial(n-1);    
+      
+    }
+    public static void main(String[] args) {
+       int ans = factorial(5);
+       System.out.println(ans );
+    }
+    
+}
