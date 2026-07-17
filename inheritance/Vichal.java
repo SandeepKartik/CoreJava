38647de3da96d486a57966aa31782075bb35e91c
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-17 20:08:56 +0530

    Example of Super keyword to access variable in inheritance

---
+++ inheritance/Vichal.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+public class Vichal {
+    String brand="BMW";
+    void start(){
+        System.out.println("Car has started ");
+    }
+}    
+class Car1 extends Vichal{
+    private String name="Audi";
+    void run(){
+        System.out.println("Car is runing");
+    }
+    void show(){
+        System.out.println(super.brand);
+        System.out.println(name);
+    }
+}
+class Main3{
+    public static void main(String[] args) {
+        Car1 c=new Car1();
+        c.show();
+        c.run();
+        c.start();
+
+    }
+}
