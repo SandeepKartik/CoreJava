a32140ccf1cddd6accf6de197eed89f9dbbb9aab
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-07-21 23:50:12 +0530

    Abstract Class and Abstract Method

---
+++ abstractConcept/Animal.java
@@ -0,0 +1,23 @@
+package abstractConcept;
+
+abstract class Animal {
+    abstract void sound();
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog start barks");
+    }
+}
+class Cat extends Animal{
+    void sound(){
+        System.out.println("Cat do meuw");
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Animal a=new Dog();
+        a.sound();
+        Cat c=new Cat();
+        c.sound();
+    }
+}
