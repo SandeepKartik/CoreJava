bdd2f1b766782e80d96cc8c92a28ae335819ec0d
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-08-06 11:57:17 +0530

    Example of var keyword  class

--- AdvancedCoreJava/VarKeyword.java
+++ AdvancedCoreJava/VarKeyword.java
@@ -6,9 +6,10 @@ public class VarKeyword {
         var y=20.20;
         var z=x+y; // compile time decide it var never exist it changed in int, String, etc during compile 
         System.out.println("Add of two number : "+z);
+        System.out.println("This is the example of var keyword");
     }
     public static void main(String[] args) {
-        VarKeyword v=new VarKeyword();
+        var v=new VarKeyword();
         v.add();
     }
    
