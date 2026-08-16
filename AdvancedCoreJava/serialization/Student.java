1ee33f9ef76a0b70ddfe33ecfbf1b77600927305
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-08-16 16:40:15 +0530

    Example of Serialization in java

---
+++ AdvancedCoreJava/serialization/Student.java
@@ -0,0 +1,24 @@
+package AdvancedCoreJava.serialization;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+public class Student implements Serializable{
+    int id;
+    String name;
+    Student(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+    
+}
+class Main{
+    public static void main(String[] args) throws Exception {
+        Student s=new Student(101, "Sam Kartik..");
+        FileOutputStream fos=new FileOutputStream("student.ser");// connect file object to outpur stream 
+        ObjectOutputStream oss=new ObjectOutputStream(fos);// Object ko out put stream se connect krna 
+        oss.writeObject(s);// serialization is done here 
+        fos.close();
+        oss.close();
+        System.out.println("The Serialization in successfully ...");
+    }
+}
