414d3823be34611a05774a5bf745b94ae711a835
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-09-04 15:10:40 +0530

    Example of Generic class with multiple key

---
+++ AdvancedCoreJava/generics/genericClass/Pair.java
@@ -0,0 +1,21 @@
+package AdvancedCoreJava.generics.genericClass;
+
+public class Pair <K,V>{
+    K key;
+    V value;
+    Pair(K key,V value){
+        this.key=key;
+        this.value=value;
+    }
+    void display(){
+        System.out.println("Key : "+key);
+        System.out.println("Value : "+value);
+    }
+    
+}
+class Test{
+    public static void main(String[] args) {
+        Pair<Integer,String>p=new Pair<Integer,String>(101, "Sam");
+        p.display();
+    }
+}
