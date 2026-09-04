3143adbdfea2119f44d709d7d7e4cdd66986e4ce
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-09-04 23:28:35 +0530

    Example of Collection

---
+++ AdvancedCoreJava/collections/CollectionDemo2.java
@@ -0,0 +1,21 @@
+package AdvancedCoreJava.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo2 {
+    public static void main(String[] args) {
+        Collection<Integer> c=new ArrayList<>();
+        c.add(101);
+        c.add(102);
+        c.add(103);
+        c.add(104);
+        c.add(105);
+
+        for(Integer num :c){
+            System.out.println(num);
+        }
+        System.out.println(c);
+    }
+    
+}
