8a156c5064f28816db73e07427c198d31edae28e
Sandeep Kartik <sandeepkartik17@gmail.com>
2026-09-10 22:04:17 +0530

    Example of Wildcard in generics

---
+++ AdvancedCoreJava/generics/wildcard/Student.java
@@ -0,0 +1,28 @@
+package AdvancedCoreJava.generics.wildcard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    static void printlist(List<?> list){
+        for(Object value :list){
+            System.out.println("Value is : "+value);
+        }
+    }
+    
+}
+class Main{
+    public static void main(String[] args) {
+        List<String> names=new ArrayList<>();
+        names.add("Sam kartik");
+        names.add("Vishal kartik");
+        names.add("Kriti singh");
+        System.out.println(names);
+        List<Integer> num=new ArrayList<>();
+        num.add(10);
+        num.add(12);
+        num.add(20);
+        System.out.println(num);
+
+    }
+}
